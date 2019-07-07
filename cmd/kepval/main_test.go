8182296dbcb51f8cd9b01a4e44c4679ca3020b7e
chuckha <ha.chuck@gmail.com>
2019-07-07 10:06:40 -0400

    Adds a test to show enhancements verify will not break
    Signed-off-by: chuckha <ha.chuck@gmail.com>

--- cmd/kepval/main_test.go
+++ cmd/kepval/main_test.go
@@ -1,19 +1,68 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
 func TestIntegration(t *testing.T) {
-	tempDir, err := ioutil.TempDir("", "test")
+	tempDir, err := ioutil.TempDir(".", "test")
 	if err != nil {
-		t.Fatal("failed to create a tempdir")
+		t.Fatalf("%+v", errors.WithStack(err))
 	}
+	defer os.RemoveAll(tempDir)
+	fmt.Println("Cloning...")
 	cmd := exec.Command("git", "clone", "https://github.com/kubernetes/enhancements")
 	cmd.Dir = tempDir
-	out, err := gitClone("https://github.com/kubernetes/enhancements", into string)
-	// git clone
-	// pass root
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		t.Fatalf("%+v", errors.WithStack(err))
+	}
+	fmt.Println("Building...")
+	cmd = exec.Command("go", "build", "-o", filepath.Join(tempDir, "kepval"), ".")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		fmt.Println(string(out))
+		t.Fatal(err)
+	}
+	fmt.Println("Walking...")
+	if filepath.Walk(
+		filepath.Join(tempDir, "enhancements", "keps"),
+		func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+			if err != nil {
+				t.Fatalf("%+v", err)
+			}
+			if ignore(info.Name()) {
+				return nil
+			}
+			cmd = exec.Command(filepath.Join(tempDir, "kepval"), path)
+			if out, err := cmd.CombinedOutput(); err != nil {
+				fmt.Println(string(out))
+				t.Fatal(err)
+			}
+			return nil
+		},
+	) != nil {
+		t.Fatal(err)
+	}
+}
+
+func ignore(name string) bool {
+	if !strings.HasSuffix(name, "md") {
+		return true
+	}
+	if name == "0023-documentation-for-images.md" {
+		return true
+	}
+	return false
 }
